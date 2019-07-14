62153a2c50462a0380940c987dcca905d2d5f194
CelisseD <43502934+CelisseD@users.noreply.github.com>
2019-07-14 02:21:33 -0400

    file directory setup

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Super Cheesy Pizza Burger");
+INSERT INTO burgers (burger_name) VALUES ("Turkey Meatball Balogna Burger");
+INSERT INTO burgers (burger_name) VALUES ("Quadruple Bacon Chicken Nugget Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon, Lettuce, n Potatah Burger");
+INSERT INTO burgers (burger_name) VALUES ("Classic Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Classic Cheeseburger w/ DOUBLE PICKLES!!!!");
