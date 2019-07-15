29dfe98583d1b0caa10d3fe26a02a1c2ba76b34b
CelisseD <43502934+CelisseD@users.noreply.github.com>
2019-07-14 20:35:22 -0400

    added code that will call ORM functions

--- models/burger.js
+++ models/burger.js
@@ -3,29 +3,24 @@ var orm = require("../config/orm.js")
 // CREATE THE CODE THAT WILL CALL THE ORM FUNCTIONS
 // USING BURGER SPECIFIC INPUT FOR THE ORM
 
-$(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
-
-    var newBurger = {
-      name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
-    };
-
-    // Send the POST request.
-    $.ajax("/api/burger", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-
-
+var burger = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
 
 // EXPORT
 module.exports = burger;
